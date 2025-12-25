a1b89fae519ffc7005b54e7a23f93660e163f2f2
purohitprince5 <princepurohit0506@gmail.com>
2025-12-25 19:10:47 +0530

    Initial backend project setup

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// Test route
+app.get('/', (req, res) => {
+  res.json({ message: 'Server started ' });
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
